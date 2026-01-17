d684f8c6631834cdd571872d92607d6d22fec31d
shyam1710 <shyamconfig@gmail.com>
2026-01-17 13:42:03 +0530

    added connection to the mongo db

---
+++ src/config/dbConfig.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connection = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to the database successfully");
+    } catch(error) {
+        console.log(error.message);
+    };
+};
+
+
+export default connection;
